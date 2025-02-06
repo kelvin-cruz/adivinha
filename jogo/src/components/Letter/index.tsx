2f19d0cdab7a279a0d2718a18be695027d8fe90f
kelvincruz <kelvincruz.dev@outlook.com>
2025-02-05 21:20:01 -0300

    reutilizando componentes e validando os valores corretos e incorretos

--- jogo/src/components/Letter/index.tsx
+++ jogo/src/components/Letter/index.tsx
@@ -2,13 +2,20 @@ import styles from './styles.module.css'
 
 type props = {
     value?: string
+    size?: "defaut" | "small"
+    color?: "defaut" | "correct" | "wrong"
 }
 
 
-export function Letter({ value = "" }: props) {
+export function Letter({ value = "", size = "defaut", color = "defaut" }: props) {
     return (
-        <div className= {styles.container}>
-            
+        <div className={`
+        ${styles.container}
+        ${size === "small" && styles.LetterSmall}
+        ${color === "correct" && styles.LetterCorrect}
+         ${color === "wrong" && styles.LetterWrong}
+        `}>
+
             <span>{value}</span>
 
         </div>
--- jogo/src/components/LettersUsed/index.tsx
+++ jogo/src/components/LettersUsed/index.tsx
@@ -1,10 +1,16 @@
 import Styles from "./styles.module.css"
+import { Letter } from "../Letter"
 
 
-export function LetterUsed(){
-    return(
+export function LetterUsed() {
+    return (
         <div className={Styles.LetterUsed}>
             <h5>Letras utilizadas</h5>
+            <div>
+                <Letter value="x" size="small" color="correct"/>
+                <Letter value="y" size="small" color="wrong"/>
+            </div>
+
         </div>
     )
 }
